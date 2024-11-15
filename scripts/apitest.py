cf11f421e7468fefc552875705191592a1e1a587
yi_t <marvelousme@163.com>
2024-11-15 18:57:47 +0800

    test

--- scripts/apitest.py
+++ scripts/apitest.py
@@ -3,8 +3,8 @@ from product_hunt_list_to_md import Product
 
 a=Product(id='11',
           name='Softr for Notion',
-          tagline='将 Notion 数据库转变为门户网站和应用程序，无需编码。',
-          description='在你的 Notion 数据库上构建自定义应用程序：比如客户和会员门户、库存跟踪器、目录等。你可以连接到 Notion 数据，定制设计和布局，设置详细的权限，然后将你的应用发布到一个自定义域名上。',
+          tagline='Discover hidden niches before they go mainstream',
+          description='A new trends intelligence tool that helps you discover under-the-radar trending products, companies and ideas. Spot new niche markets and uncover hidden business opportunities before they go mainstream',
           votesCount=1,
           createdAt='2023-10-05T14:48:00Z',
           featuredAt='2023-10-05T14:48:00Z',
